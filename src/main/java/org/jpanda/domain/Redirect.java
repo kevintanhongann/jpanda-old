0dedcba0b2bb5b917853e9e20e98e2e8ecd90735
T-PWK <tompwk@gmail.com>
2015-01-21 19:38:19 +0000

    Domain files
    Post entity class added
    Redirect entity class and type enum added

---
+++ src/main/java/org/jpanda/domain/Redirect.java
@@ -0,0 +1,41 @@
+package org.jpanda.domain;
+
+/**
+ * Created by tbs on 21/01/2015.
+ */
+public class Redirect
+{
+    private String toUrl;
+    private String fromUrl;
+    private RedirectType type;
+
+    public RedirectType getType()
+    {
+        return type;
+    }
+
+    public void setType(RedirectType type)
+    {
+        this.type = type;
+    }
+
+    public String getTo()
+    {
+        return toUrl;
+    }
+
+    public void setTo(String to)
+    {
+        this.toUrl = to;
+    }
+
+    public String getFrom()
+    {
+        return fromUrl;
+    }
+
+    public void setFrom(String from)
+    {
+        this.fromUrl = from;
+    }
+}
---
+++ src/main/java/org/jpanda/domain/RedirectType.java
@@ -0,0 +1,19 @@
+package org.jpanda.domain;
+
+public enum RedirectType
+{
+    /**
+     *
+     */
+    HTTP_301,
+
+    /**
+     *
+     */
+    HTTP_302,
+
+    /**
+     *
+     */
+    INTERNAL
+}
