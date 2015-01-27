83a95b2c6e49947846eb5efff6579680edef26ac
T-PWK <tompwk@gmail.com>
2015-01-27 19:41:20 +0000

    ResourceNotFound exception takes error message during construction

--- src/main/java/org/jpanda/web/ResourceNotFoundException.java
+++ src/main/java/org/jpanda/web/ResourceNotFoundException.java
@@ -11,4 +11,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException
 {
+    public ResourceNotFoundException(String message)
+    {
+        super(message);
+    }
+
+    public ResourceNotFoundException()
+    {
+    }
 }
