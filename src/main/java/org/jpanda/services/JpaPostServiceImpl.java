317fa3719e5bc649fc3381861cbafb83b9f88d42
T-PWK <tompwk@gmail.com>
2015-01-29 22:58:35 +0000

    API Post controller and Post service changes

--- src/main/java/org/jpanda/services/JpaPostServiceImpl.java
+++ src/main/java/org/jpanda/services/JpaPostServiceImpl.java
@@ -2,35 +2,63 @@ package org.jpanda.services;
 
 import org.jpanda.config.ApplicationProperties;
 import org.jpanda.domain.Post;
+import org.jpanda.domain.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.Date;
 
 @Service
 public class JpaPostServiceImpl implements PostService
 {
     @Autowired
+    private PostRepository repository;
+
+    //    private static final List<Post> posts = new ArrayList<>();
+//    static
+//    {
+//        createPost("sailsjs-v010-express-custommiddleware", "Sails.js v0.10 express customMiddleware", "In this post, I would like to share how to customize Express server in Sails.js version 0.10. Handling of the customMiddleware has slightly changed from Sails v0.9.x. In version 0.10 that method needs to be configured in http hook (not express hook, as in the");
+//        createPost("sailsjs-v010-express-custommiddleware-2", "Sails.js v0.10 express customMiddleware 2", "In this post, I would like to share how to customize Express server in Sails.js version 0.10. Handling of the customMiddleware has slightly changed from Sails v0.9.x. In version 0.10 that method needs to be configured in http hook (not express hook, as in the");
+//        createPost("sailsjs-v010-express-custommiddleware-2", "Sails.js v0.10 express customMiddleware 3", "In this post, I would like to share how to customize Express server in Sails.js version 0.10. Handling of the customMiddleware has slightly changed from Sails v0.9.x. In version 0.10 that method needs to be configured in http hook (not express hook, as in the");
+//        createPost("sailsjs-v010-express-custommiddleware-2", "Sails.js v0.10 express customMiddleware 4", "In this post, I would like to share how to customize Express server in Sails.js version 0.10. Handling of the customMiddleware has slightly changed from Sails v0.9.x. In version 0.10 that method needs to be configured in http hook (not express hook, as in the");
+//        createPost("sailsjs-v010-express-custommiddleware-2", "Sails.js v0.10 express customMiddleware 5", "In this post, I would like to share how to customize Express server in Sails.js version 0.10. Handling of the customMiddleware has slightly changed from Sails v0.9.x. In version 0.10 that method needs to be configured in http hook (not express hook, as in the");
+//        createPost("sailsjs-v010-express-custommiddleware-2", "Sails.js v0.10 express customMiddleware 6", "In this post, I would like to share how to customize Express server in Sails.js version 0.10. Handling of the customMiddleware has slightly changed from Sails v0.9.x. In version 0.10 that method needs to be configured in http hook (not express hook, as in the");
+//        createPost("sailsjs-v010-express-custommiddleware-2", "Sails.js v0.10 express customMiddleware 7", "In this post, I would like to share how to customize Express server in Sails.js version 0.10. Handling of the customMiddleware has slightly changed from Sails v0.9.x. In version 0.10 that method needs to be configured in http hook (not express hook, as in the");
+//        createPost("sailsjs-v010-express-custommiddleware-2", "Sails.js v0.10 express customMiddleware 8", "In this post, I would like to share how to customize Express server in Sails.js version 0.10. Handling of the customMiddleware has slightly changed from Sails v0.9.x. In version 0.10 that method needs to be configured in http hook (not express hook, as in the");
+//        createPost("sailsjs-v010-express-custommiddleware-2", "Sails.js v0.10 express customMiddleware 9", "In this post, I would like to share how to customize Express server in Sails.js version 0.10. Handling of the customMiddleware has slightly changed from Sails v0.9.x. In version 0.10 that method needs to be configured in http hook (not express hook, as in the");
+//        createPost("sailsjs-v010-express-custommiddleware-2", "Sails.js v0.10 express customMiddleware 10", "In this post, I would like to share how to customize Express server in Sails.js version 0.10. Handling of the customMiddleware has slightly changed from Sails v0.9.x. In version 0.10 that method needs to be configured in http hook (not express hook, as in the");
+//    }
+//
+//    private static void createPost(String slug, String title, String teaser)
+//    {
+//        final Post post = new Post();
+//        post.setSlug(slug);
+//        post.setTitle(title);
+//        post.setTeaserText(teaser);
+//
+//        posts.add(post);
+//    }
+//
+    @Autowired
     private ApplicationProperties config;
 
     @Override
     public Page<Post> findAll(int pageNum)
     {
-        return new PageImpl<Post>(Collections.emptyList());
+        return repository.findAll(new PageRequest(pageNum, config.getPostsPerPage()));
     }
 
     @Override
     public Page<Post> findByDateRange(Date start, Date end, int pageNum)
     {
-        return new PageImpl<Post>(Collections.emptyList());
+        return repository.findAll(new PageRequest(pageNum, config.getPostsPerPage()));
     }
 
     @Override
     public Post findBySlug(String slug)
     {
-        return null;
+        return repository.findOne(1L);
     }
 }
--- src/main/java/org/jpanda/web/api/v1/PostApiController.java
+++ src/main/java/org/jpanda/web/api/v1/PostApiController.java
@@ -2,12 +2,17 @@ package org.jpanda.web.api.v1;
 
 import org.jpanda.domain.Post;
 import org.jpanda.domain.PostRepository;
+import org.jpanda.web.DuplicateEntityException;
+import org.jpanda.web.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -20,6 +25,77 @@ public class PostApiController
     public Page<Post> allRedirects(final Pageable pageable)
     {
         return repository.findAll(pageable);
+    }
+
+    /**
+     * Create new {@code Post} instance
+     *
+     * @param post   {@code Post} instance to be created
+     * @param result input {@code Post} instance validation results
+     * @return newly created {@code Post} instance
+     * @throws BindException            if there was any validation issue with the input {@code Post}
+     * @throws DuplicateEntityException if there is a redirect entity with the same {@code Post#getSlug slug}
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post create(@RequestBody @Valid final Post post, final BindingResult result)
+            throws BindException, DuplicateEntityException
+    {
+        if (result.hasErrors())
+        {
+            throw new BindException(result);
+        }
+
+        if (repository.findBySlug(post.getSlug()) != null)
+        {
+            throw new DuplicateEntityException(String.format("Post for [%s] already exists", post.getSlug()));
+        }
+
+        return repository.save(post);
+    }
+
+
+    /**
+     * Returns {@link org.jpanda.domain.Post} instance by the given entity identifier
+     *
+     * @param id entity identifier
+     * @return {@link org.jpanda.domain.Post} instance
+     * @throws org.jpanda.web.ResourceNotFoundException if there is no instance with the given identifier
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Post findById(@PathVariable("id") final long id)
+    {
+        checkPost(id);
+        return repository.findOne(id);
+    }
+
+
+    /**
+     * Deletes {@code Post} instance by the given entity identifier
+     *
+     * @param id entity identifier
+     * @throws org.jpanda.web.ResourceNotFoundException if there is no instance with the given identifier
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") final long id)
+    {
+        checkPost(id);
+        repository.delete(id);
+    }
+
+    /**
+     * Checks if a post entity with the given identifier exists.
+     *
+     * @param id post entity identifier
+     * @throws org.jpanda.web.ResourceNotFoundException if {@link org.jpanda.domain.Post} entity does not exist
+     *                                                  for the given identifier
+     */
+    private void checkPost(final long id)
+    {
+        if (!repository.exists(id))
+        {
+            throw new ResourceNotFoundException(String.format("No post with id:[%d] exists!", id));
+        }
     }
 
 }
