0940297d188319d1721ba7e7bb5c8898c2d6dc68
Tom Pawlak <tompwk@gmail.com>
2015-02-18 00:04:01 +0000

    Admin application template changes

--- src/main/resources/public/admin/js/application.js
+++ src/main/resources/public/admin/js/application.js
@@ -1,6 +1,6 @@
-var panda = angular.module('panda', ['ngRoute']);
+var panda = angular.module('panda', ['panda.config', 'ngRoute', 'ngResource']);
 
-panda.config(['$routeProvider', function($route){
+panda.config(['$routeProvider', '$httpProvider', 'Security', function($route, $http, Security){
     $route
         .when('/', { templateUrl:'/admin/partial/index', controller: 'IndexCtrl' })
         .when('/profile', { templateUrl:'/admin/partial/profile', controller: 'ProfileCtrl' })
@@ -11,11 +11,15 @@ panda.config(['$routeProvider', function($route){
         .when('/admin/redirects', { templateUrl:'/admin/partial/redirects', controller: 'RedirectsCtrl' })
         .when('/analytics', { templateUrl:'/admin/partial/analytics', controller: 'AnalyticsCtrl' })
         .otherwise({ redirectTo: '/' });
+    $http.defaults.headers.common = { 'X-CSRF-TOKEN': Security.token };
 }]);
 
 panda.controller('NavCtrl', ['$scope', function ($scope) {
     $scope.$root.$on('nav:head', function(event, header){
         $scope.header = header;
+    });
+    $scope.$root.$on('nav:crumb', function(event, breadcrumb){
+        $scope.breadcrumb = breadcrumb;
     });
     $scope.is = function(header) {
         return $scope.header === header;
@@ -24,31 +28,66 @@ panda.controller('NavCtrl', ['$scope', function ($scope) {
 
 panda.controller('IndexCtrl', ['$scope', function ($scope) {
     $scope.$root.$emit('nav:head', 'index');
+    $scope.$root.$emit('nav:crumb');
 }]);
 
 panda.controller('ProfileCtrl', ['$scope', function ($scope) {
+    $scope.$root.$emit('nav:crumb', ['Profile']);
 }]);
 
 panda.controller('PagesCtrl', ['$scope', function ($scope) {
     $scope.$root.$emit('nav:head', 'content');
+    $scope.$root.$emit('nav:crumb', ['Content', 'Pages']);
 }]);
 
 panda.controller('AnnouncementCtrl', ['$scope', function ($scope) {
     $scope.$root.$emit('nav:head', 'content');
+    $scope.$root.$emit('nav:crumb', ['Content', 'Announcement']);
 }]);
 
 panda.controller('UsersCtrl', ['$scope', function ($scope) {
     $scope.$root.$emit('nav:head', 'admin');
+    $scope.$root.$emit('nav:crumb', ['Administration', 'Users']);
 }]);
 
 panda.controller('GroupsCtrl', ['$scope', function ($scope) {
     $scope.$root.$emit('nav:head', 'admin');
+    $scope.$root.$emit('nav:crumb', ['Administration', 'Groups']);
 }]);
 
-panda.controller('RedirectsCtrl', ['$scope', function ($scope) {
+panda.controller('RedirectsCtrl', ['$scope', 'Redirects', function ($scope, Redirects) {
     $scope.$root.$emit('nav:head', 'admin');
+    $scope.$root.$emit('nav:crumb', ['Administration', 'Redirects']);
+    
+    $scope.sizes = [10, 20, 50, 100];
+    $scope.size = 10;
+    
+    $scope.setSize = function(size) {
+        $scope.size = size;
+        $scope.reload();
+    };
+    
+    $scope.remove = function(item){
+        Redirects.remove({id:item.id}).$promise.then($scope.reload);
+    };
+    
+    $scope.reload = function() {
+        $scope.loading = true;
+        Redirects.get({ size: $scope.size }, function(data){
+             $scope.redirects = data;
+             $scope.loading = false;
+        });
+    };
+
+    $scope.reload();
+    $('.dropdown').dropdown();
 }]);
 
 panda.controller('AnalyticsCtrl', ['$scope', function ($scope) {
     $scope.$root.$emit('nav:head', 'analytics');
+    $scope.$root.$emit('nav:crumb', ['Analytics']);
+}]);
+
+panda.factory('Redirects',['$resource', function($resource){
+    return $resource('/api/v1/redirects/:id', { id: '@id' });
 }]);
